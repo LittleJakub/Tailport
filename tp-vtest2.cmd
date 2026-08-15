ec47552cd807cba36fbd9e633498b09cf30bf5d3
LittleJakub <35995921+LittleJakub@users.noreply.github.com>
2026-08-15 23:55:54 +0800

    remove stray diagnostic file

--- tp-vtest2.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-for /f "tokens=1,2,3 delims=<>" %%A in ('echo ^<Version^>1.8.0^</Version^>') do echo BOTH: A=[%%A] B=[%%B] C=[%%C]
-for /f "tokens=1,2 delims=<" %%A in ('echo ^<Version^>1.8.0^</Version^>') do echo LT:   A=[%%A] B=[%%B]
-for /f "tokens=1,2 delims=>" %%A in ('echo ^<Version^>1.8.0^</Version^>') do echo GT:   A=[%%A] B=[%%B]
-for /f "tokens=2 delims=<>" %%V in ('echo Version^>1.8.0^</Version^>') do echo NOTOKEN: V=[%%V]
