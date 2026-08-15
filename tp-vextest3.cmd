1aba64e5a0ec317efdfcafd8eac038e871538e95
LittleJakub <35995921+LittleJakub@users.noreply.github.com>
2026-08-16 00:36:45 +0800

    remove stray probe script

--- tp-vextest3.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-for /f "tokens=1,2 delims==" %%V in ('echo a=b=c') do echo T12: V=[%%V] W=[%%W]
-for /f "tokens=1,* delims==" %%V in ('echo a=b=c') do echo T1S: V=[%%V] W=[%%W]
-for /f "tokens=1,* delims==" %%V in ('echo     ^<Version^>1.8.1^</Version^>') do echo ANGL: V=[%%V] W=[%%W]
